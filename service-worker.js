11eb49e48e28fab5f6fe09eb9e13df77cf487c2e
Ajin k j <61120485+Ajinkj@users.noreply.github.com>
2023-09-26 19:44:14 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+// service-worker.js
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('drama-sounds-cache').then(function(cache) {
+      return cache.addAll([
+        './',
+        'index.html',
+        'background1.mp3',
+        'background2.mp3',
+        'tune1.mp3',
+        'tune2.mp3',
+        'special1.mp3',
+        'special2.mp3'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
